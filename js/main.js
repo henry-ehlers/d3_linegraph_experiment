a50270963ac4520c678f213e716f9e91c89eb7d7
Henry Ehlers <henry.ward.ehlers@gmail.com>
2023-08-18 12:27:00 +0200

    filtered out missing data

--- js/main.js
+++ js/main.js
@@ -26,35 +26,44 @@ let y = d3.scaleLinear()
 let yAxis = d3.axisLeft(y);
 
 Promise.all(promises).then(function(promisedData){
-    let weights = promisedData[0];
+    const weights = promisedData[0];
+    
+    // Format data to match actual types required
     let timeParser = d3.timeParse('%m/%d/%Y')
     weights.forEach(d => {
         d.Date = timeParser(d.Date);
         d.BMI = Number(d.BMI);    
     });
-    console.log(weights[0]);
+    console.log(weights);
+    const cleanWeights = weights.filter(d => d.BMI > 0) 
+    console.log(cleanWeights);
 
+    // Create svg in chart area
     let svg = d3.select("#chart-area")
         .append('svg')
             .attr('width', FIGURE.WIDTH)
             .attr('height', FIGURE.HEIGHT);
 
+    // Create group for actual plot area within margins
     let plot = svg.append('g')
         .attr('transform', `translate(${MARGINS.LEFT}, ${MARGINS.TOP})`);
     
-    x.domain(d3.extent(weights.map(d => d.Date)));
+    // Include actual data into x scale
+    x.domain(d3.extent(cleanWeights.map(d => d.Date)));
     plot.append('g')
         .attr('class', 'x-axis')
         .attr('transform', `translate(0, ${PLOT.HEIGHT})`)
         .call(xAxis);
 
-    y.domain([0, d3.max(weights.map(d => d.BMI))])
+    // Include actual data into y scale
+    y.domain([0, d3.max(cleanWeights.map(d => d.BMI))])
     plot.append('g')
         .attr('class', 'y-axis')
         .call(yAxis);
 
+    // Join and enter data as circles
     plot.selectAll('circle')
-        .data(weights)
+        .data(cleanWeights)
         .enter()
         .append('circle')
             .attr('cx', d => x(d.Date))
