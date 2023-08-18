8f5fa5db68daa6e2a4df211f50e38db895e2351d
Henry Ehlers <henry.ward.ehlers@gmail.com>
2023-08-18 11:40:30 +0200

    formatting

--- js/main.js
+++ js/main.js
@@ -2,10 +2,19 @@ const promises = [
     d3.csv("./data/daily_weight.csv")
 ];
 
+// let xAxis = d3.axisBottom()
+//     .domain(d3.dateRange())
 
 Promise.all(promises).then(function(promisedData){
-    let weights = promisedData[0]
-    console.log(weights)
+    let weights = promisedData[0];
+    let timeParser = d3.timeParse('%m/%d/%Y')
+    weights.forEach(d => {
+        d.Date = timeParser(d.Date);
+        d.BMI = Number(d.BMI);    
+    });
+    console.log(weights[0]);
+
+
 }).catch(function(error){
     console.log(error)
-})
+});
